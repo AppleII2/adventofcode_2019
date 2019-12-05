fd075d45da6d8f81567c15c626f564a229a1bab8
Nelson Healy <nelson@atom.industries>
2019-12-04 22:16:04 -0500

    part 2 day 4

--- 04.py
+++ 04.py
@@ -1,9 +1,10 @@
 import re
+from collections import Counter
 lower = 138241
 upper = 674034
 
 
-def part1(lower, upper):
+def part1(lower=lower, upper=upper):
     matches = []
     for n in range(lower, upper+1):
         n = str(n)
@@ -12,3 +13,12 @@ def part1(lower, upper):
                 matches.append(n)
     print(len(matches))
     return matches
+
+def part2(matches):
+    # This may be a faster way to do part1 as well...
+    filtered = []
+    for match in matches:
+        if 2 in Counter(list(match)).values():
+            filtered.append(match)
+    print(len(filtered))
+    return filtered
